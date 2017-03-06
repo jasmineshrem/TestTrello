a70f87ce7199171662e8b3b9b7c20c21a8746b47
jakobpederson <jakob.pederson+github@gmail.com>
2017-03-06 16:18:43 -0600

    Update trello_class
    Modified to function better.

--- trello_class.py
+++ trello_class.py
@@ -25,34 +25,37 @@ class TrelloClass:
     def create_board(self, board_name):
         return self.client.add_board(board_name)
 
-    def close_board(self, board_name):
-        self.get_board(board_name).close()
+    def close_board(self, board):
+        board.close()
 
-    def get_open_lists(self, board_name):
-        return self.get_board(board_name).open_lists()
+    def get_open_lists(self, board):
+        return board.open_lists()
 
-    def create_list(self, board_name, list_name):
-        return self.get_board(board_name).add_list(list_name)
+    def create_list(self, board, list_name):
+        return board.add_list(list_name)
 
-    def get_list(self, board_name, list_name):
-        return next(self.get_list_gen(board_name, list_name))
+    def get_list(self, board, list_name):
+        return next(self.get_list_gen(board, list_name))
 
-    def get_list_gen(self, board_name, list_name):
-        for trello_list in self.get_open_lists(board_name):
+    def get_list_gen(self, board, list_name):
+        for trello_list in self.get_open_lists(board):
             if trello_list.name == list_name:
                 yield trello_list
 
-    def close_list(self, board_name, list_name):
-        self.get_list(board_name, list_name).close()
+    def close_list(self, board, list_name):
+        self.get_list(board, list_name).close()
 
-    def get_cards_on_board(self, board_name):
-        return self.get_board(board_name).open_cards()
+    def get_cards_on_board(self, board):
+        return board.open_cards()
 
-    def get_cards_on_list(self, board_name, list_name):
-        return self.get_list(board_name, list_name).list_cards()
+    def get_cards_on_list(self, board, list_name):
+        return self.get_list(board, list_name).list_cards()
 
-    def add_card(self, board_name, list_name, card_name):
-        return self.get_list(board_name, list_name).add_card(card_name)
+    def create_card(self, board, trello_list, card_name, source=None):
+        if source:
+            return board.get_list(trello_list.id).add_card(card_name, source=source)
+        else:
+            return board.get_list(trello_list.id).add_card(card_name)
 
     def get_card(self, card_name, board_name):
         return next(self.get_card_gen(card_name, board_name))
@@ -61,3 +64,6 @@ class TrelloClass:
         for card in self.get_board(board_name).open_cards():
             if card.name == card_name:
                 yield card
+
+    def delete_card(self, card):
+        self.client.get_card(card.id)
